2f9f399f32f268cf9be9baf7675c29c738079715
trossjo <joeulhan@naver.com>
2020-07-20 03:51:23 +0900

    Add All

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all: add-nbo
 
 add-nbo: add-nbo.c
-        g++ -o add-nbo add-nbo.c
+	g++ -o add-nbo add-nbo.c
--- add-nbo.c
+++ add-nbo.c
@@ -8,30 +8,30 @@
 
 int main(int argc, char *argv[])
 {
-        int i = argc;
-        if (argc == 3 ) {
-                FILE *fp1, *fp2;
-                uint32_t f1;
-                uint32_t f2;
-                uint32_t sum;
+	int i = argc;
+	if (argc == 3 ) {
+        	FILE *fp1, *fp2;
+		uint32_t f1;
+		uint32_t f2;
+		uint32_t sum;
 
-                fp1 = fopen(argv[1],"rb");
-                fp2 = fopen(argv[2],"rb");
-                fread(&f1, sizeof(uint32_t), 1, fp1);
-                fread(&f2, sizeof(uint32_t), 1, fp2);
+	        fp1 = fopen(argv[1],"rb");
+		fp2 = fopen(argv[2],"rb");
+		fread(&f1, sizeof(uint32_t), 1, fp1);
+		fread(&f2, sizeof(uint32_t), 1, fp2);
 
-                sum = ntohl(f1)+ntohl(f2);
-                
-                printf("%d(0x%X) + ",ntohl(f1), ntohl(f1));
-                printf("%d(0x%X) = ",ntohl(f2), ntohl(f2));
-                printf("%d(ox%X)", ntohl(f1)+ntohl(f2),ntohl(f1)+ntohl(f2)>
-                printf("\n");
-                fclose(fp1);
-                fclose(fp2);
-                return 0;
-        }
-        else {
-                printf("Usage : %s <filename1> <filename2>\n", argv[0]);
-                return 0;
-        }
+		sum = ntohl(f1)+ntohl(f2);
+
+		printf("%d(0x%X) + ",ntohl(f1), ntohl(f1));
+		printf("%d(0x%X) = ",ntohl(f2), ntohl(f2));
+		printf("%d(ox%X)", ntohl(f1)+ntohl(f2),ntohl(f1)+ntohl(f2));
+		printf("\n");
+		fclose(fp1);
+		fclose(fp2);
+		return 0;
+	}
+	else {
+		printf("Usage : %s <filename1> <filename2>\n", argv[0]);
+		return 0;
+	}
 }
